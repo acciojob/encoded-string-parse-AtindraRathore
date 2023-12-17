8e5cdc93fc5b47a29a6c78777dc5d99f0c4042d4
Atindra Singh Rathore <118996145+AtindraRathore@users.noreply.github.com>
2023-12-17 20:16:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const parseCode = (str) => {
   // your code here
+	 let [firstname,lastname,id] = str.split("000");
+
+		console.log(firstname);
+		console.log(lastname);
+		console.log(id);
+	   let obj={
+	       firstname:firstname,
+	       lastname:lastname,
+	       id:id
+	   }
+	   return obj;
+		
 };
 
 // Do not change the code below
-const str = prompt("Enter str: ");
-alert(JSON.stringify(parseCode(str)));
+// const str = prompt("Enter str: ");
+// alert(JSON.stringify(parseCode(str)));
